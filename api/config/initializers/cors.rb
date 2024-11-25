2dd93e1dc52d38f627a0fb1ab1125fa2cd204cff
Pretty-dogg <haruki89618@gmail.com>
2024-11-25 21:05:45 +0900

    11/25_DB_edit
    database名を間違えて修正していたため、復元。

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://chamomile-app-1.onrender.com' 
+    origins 'https://chamomile-app-1.onrender.com', 'http://localhost:3000'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
