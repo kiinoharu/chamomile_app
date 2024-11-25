3939fad5e6955071a03f8d00b826e837770a23fa
Pretty-dogg <haruki89618@gmail.com>
2024-11-25 11:02:42 +0900

    11/25_deploy_edit

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000' 
+    origins 'https://chamomile-app-1.onrender.com' 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
